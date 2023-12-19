a36da48fa5d251f79cb9275a5fadb9d36c9da811
Ben <ben.visintainer@me.com>
2023-12-18 22:14:46 -0700

    Added unit tests and refactored solution structure to better support test project in a sub folder.

---
+++ Tests/DocumentTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+
+using TeqBench.System.Data.NoSql.MongoDB.Models;
+
+namespace TeqBench.System.Data.NoSql.MongoDB.Models.Tests;
+
+[TestClass]
+public class DocumentTests
+{
+    [TestMethod]
+    public void TestPropertyDefaults()
+    {
+        var mock = new Mock<Document>();
+        mock.SetupAllProperties();
+
+        IDocument doc = mock.Object;
+        Assert.IsNull(doc.Id);
+        Assert.AreEqual(doc.CreatedAt, DateTime.MinValue);
+    }
+
+    [TestMethod]
+    public void TestId()
+    {
+        var mock = new Mock<Document>("64c0a0ab8e22874a66eadb4c");
+
+        IDocument doc = mock.Object;
+        Assert.AreEqual(doc.Id, "64c0a0ab8e22874a66eadb4c");
+    }
+
+    [TestMethod]
+    public void TestCreatedAt()
+    {
+        var mock = new Mock<Document>("64c0a0ab8e22874a66eadb4c");
+
+        IDocument doc = mock.Object;
+        Assert.AreNotEqual(doc.CreatedAt, DateTime.MinValue);
+    }
+}
